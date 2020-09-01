6e399cf33651d2d744b0a97171305aad05a0aa58
jpassgo <jeffpascoe9@gmail.com>
2020-08-31 20:06:03 -0500

    Selection sort implementation.

---
+++ sorting/SelectionSort.py
@@ -0,0 +1,12 @@
+
+def selection_sort(array):
+    for marker in range(len(array)):
+        for index in range(marker, len(array)):
+            if array[marker] > array[index]:
+                array[marker], array[index] = array[index], array[marker]    
+
+l = [18, 9, 2, 2323, 5, 3, 4, 1, 98, 2, 323]
+
+print(l)
+selection_sort(l)
+print(l)
