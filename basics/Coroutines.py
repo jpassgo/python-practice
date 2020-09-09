2f1161b502d308ec15262567813db9080ec0368c
jpassgo <jeffpascoe9@gmail.com>
2020-09-09 18:35:47 -0500

    Merge sort implementation completed.

---
+++ basics/Coroutines.py
@@ -0,0 +1,14 @@
+
+def my_coroutine():
+    try:
+        value = (yield)
+        print(value)
+    except GeneratorExit:
+        print('Exiting coroutine')
+
+coroutine = my_coroutine()
+next(coroutine)
+
+coroutine.send("Hello")
+
+
--- sorting/MergeSort.py
+++ sorting/MergeSort.py
@@ -1,26 +1,33 @@
-def merge_sort(array1, array2):
+def merge(array1, array2):
     sorted_array = []
     i, j = 0, 0
     while i < len(array1) and j < len(array2):
-        print(f"Left list index i is {i} with value of {array1[i]}")
-        print(f"Left list index j is {j} with value of {array2[j]}")
         if array1[i] < array2[j]:
             sorted_array.append(array1[i])
             i += 1
         else:
             sorted_array.append(array2[j])
             j += 1
-        
-    while j < len(array2):
-        sorted_array.append(array2[j])
-        j += 1
     while i < len(array1):
         sorted_array.append(array1[i])
         i += 1
+    while j < len(array2):
+        sorted_array.append(array2[j])
+        j += 1
 
     return sorted_array
 
-l1 = [1, 2, 3, 6, 7, 9, 10]
-l2 = [19, 999, 2323432]
+def merge_sort(array):
+    if len(array) < 2:
+        return array[:]
+    else:
+        middle = len(array)//2
+        l1 = merge_sort(array[:middle])
+        l2 = merge_sort(array[middle:])
+        return merge(l1, l2)
 
-print(merge_sort(l1, l2))
+
+l3 = [900, 89, 1, 323, 4, 5, 6, 8998]
+
+sortedList = merge_sort(l3)
+print(sortedList)
