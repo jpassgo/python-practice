5856f7415f81449d45e338ab377aa01bf612f79c
jpassgo <jeffpascoe9@gmail.com>
2020-09-11 18:56:03 -0500

    Quicksort implementation.

---
+++ sorting/QuickSort.py
@@ -0,0 +1,19 @@
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[-1]
+        smaller, equal, larger = [], [], []
+        for num in array:
+            if num < pivot:
+                smaller.append(num)
+            elif num == pivot:
+                equal.append(num)
+            else:
+                larger.append(num)
+    return quicksort(smaller) + equal + quicksort(larger)
+
+
+l = [32, 2, 1, 434, 29, 100, 99, 2, 3, 89898, 23]
+print(quicksort(l))
