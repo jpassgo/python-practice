ba686d74a0e6cef54bbe95d3cf35628e3dcb7519
jpassgo <jeffpascoe9@gmail.com>
2020-09-17 20:52:21 -0500

    Adding some comments.

--- datastructures/HashTable.py
+++ datastructures/HashTable.py
@@ -4,9 +4,12 @@ class HashTable:
         self.size = size
         self.hash_table = self.create_buckets()
 
+    # Creates a list of tuples containing a key and a value
     def create_buckets(self):
         return [[] for _ in range(self.size)]
 
+    # Checks to see if an item with given key exists and updates that key if found. 
+    # If key is new then that key value pair is added.
     def set_value(self, key, value):
         hashed_key = hash(key)%self.size
         bucket = self.hash_table[hashed_key]
@@ -26,6 +29,7 @@ class HashTable:
     def get_value(self, key):
         pass
 
+    # Loops through tuples in list and prints them all.
     def __str__(self):
         return "".join(str(item) for item in self.hash_table)
 
