9ff5fb859d8a419daa327e48bea8f9f8c863ed46
jpassgo <jeffpascoe9@gmail.com>
2020-09-30 21:23:31 -0500

    Vehicle interface.

---
+++ oop/Vehhicle.py
@@ -0,0 +1,23 @@
+class Vehicle:
+
+    def __init__(self):
+        pass
+
+    def enter_vehicle(self):
+        pass
+
+    def exit_vehicle(self):
+        pass
+
+    def begin_acceleration(self):
+        pass
+
+    def decelerate(self):
+        pass
+
+    def turn(self, direction):
+        pass
+
+    def currentSpeed(self):
+        pass
+    
