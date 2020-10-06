2a2ab996866c25c0e3c4bea29946ecc0aea8e723
jpassgo <jeffpascoe9@gmail.com>
2020-10-05 20:10:34 -0500

    Example of using mongoengine as our ORM to store content in MongoDB.

---
+++ nosql/MongoORM.py
@@ -0,0 +1,22 @@
+from mongoengine import *
+import datetime
+
+class Post(Document):
+    title = StringField(required=True, max_length=200)
+    content = StringField(required=True)
+    author = StringField(required=True, max_length=50)
+    published = DateTimeField(default=datetime.datetime.now)
+
+connect('mongoengine_test', host='localhost', port=27017)
+
+post = Post(
+    title = 'Learning MongoDb',
+    content = 'Here is some content',
+    author = 'Jeff'
+)
+
+post.save()
+print(f"Title: {post.title}")
+post.title = 'This is the new updated content'
+post.save()
+print(f"Title: {post.title}")
