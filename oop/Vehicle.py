5eeb963547713e2835cb2df6abf19cf311877155
jpassgo <jeffpascoe9@gmail.com>
2020-10-06 20:17:07 -0500

    Making use of ABC to make Vehicle a true Abstract Base Class.

---
+++ oop/Vehicle.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod, abstractproperty
+from typing import Any
+
+class Vehicle(ABC):
+
+
+
+    @abstractmethod
+    def enter_vehicle(self) -> None:
+        pass
+
+    @abstractmethod
+    def exit_vehicle(self) -> None:
+        pass
+
+    @abstractmethod
+    def begin_acceleration(self) -> None:
+        pass
+
+    @abstractmethod
+    def decelerate(self) -> None:
+        pass
+
+    @abstractmethod
+    def turn(self, direction) -> None:
+        pass
+
+    @abstractmethod
+    def current_speed(self) -> None:
+        pass
+    
