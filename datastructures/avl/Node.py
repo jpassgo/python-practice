d8d6d275199f76c28b43e5803e3e0d57340597cf
jpassgo <jeffpascoe9@gmail.com>
2020-10-28 21:14:32 -0500

    Adding height field.

--- datastructures/avl/Node.py
+++ datastructures/avl/Node.py
@@ -2,5 +2,5 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.left = None
-        self.left = None
+        self.right = None
         self.height = 0
--- datastructures/avl/avltree.py
+++ datastructures/avl/avltree.py
@@ -5,7 +5,6 @@ class AVLTree:
     def __init__(self):
         self.root = None
 
-
     def insert(self, node):
         if self.root == None:
             self.root = node
@@ -14,11 +13,13 @@ class AVLTree:
 
     def _insert(self, curr, node):
         if node.data < curr.data:
-            if curr.left == None:
+            node.height += 1 
+            if curr.left == None:    
                 curr.left = node
             else:
                 self._insert(curr.left, node)
         elif node.data >= curr.data:
+            node.height += 1
             if curr.right == None:
                 curr.right = node
             else:
@@ -32,5 +33,5 @@ class AVLTree:
 
 
 
-    
+
         
