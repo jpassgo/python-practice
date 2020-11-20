1ad018fcd5eeac5c0869b38b2a347571f264abcb
jpassgo <jeffpascoe9@gmail.com>
2020-11-19 19:17:36 -0600

    Pulling out email validation

--- regex/testing_regex.py
+++ regex/testing_regex.py
@@ -6,14 +6,5 @@ def testing_split():
     formatted_word = ''.join(word for word in words)
     print(formatted_word)
 
-# Valid email format
-# Email consists of two parts the `Local part` and the `Domain part`
-# The local part can be up to 64 characters in length and consist of any combination of alphabetic characters, digits, or any of the following special characters:
-# ! $ % & ‘ * + – / = ? ^ _ ` . { | } ~
-# The domain part cannot be more than 255 characters in length and must conform to the specification for hostnames which is a list of dot-separated DNS labels.
-# Each DNS label must not exceed 63 characters and should consist of any
-# combination of alphabetic characters, digits and hypens.
 
 
-def validate_email(email):
-    pass
