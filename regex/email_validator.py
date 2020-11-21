0410bcf340809fd5a895aa3a3d6c36b443d0e026
jpassgo <jeffpascoe9@gmail.com>
2020-11-21 14:30:58 -0600

    Formatting

--- regex/email_validator.py
+++ regex/email_validator.py
@@ -7,26 +7,32 @@ import re
 # The domain part cannot be more than 255 characters in length and must conform to the specification for hostnames which is a list of dot-separated DNS labels.
 # Each DNS label must not exceed 63 characters and should consist of any
 # combination of alphabetic characters, digits and hypens.
+
+
 def is_email_valid(email):
     email_parts = re.split('@', email)
     local_part = email_parts[0]
     domain_part = email_parts[1]
     # print(f"local_part= {local_part} : domain_part= {domain_part}")
-    
+
     if(is_local_part_valid(local_part)):
         return True
     else:
         return False
-    
-    
+
+
 def is_local_part_valid(local_part):
     if(is_above_character_limit(local_part, 64)):
         return False
     else:
-        return re.match('^[A-Za-z0-9!$%&‘*+–/=?^_`.{|}~]', local_part) is not None 
+        return re.match(
+            '^[A-Za-z0-9!$%&‘*+–/=?^_`.{|}~]', local_part) is not None
+
 
 def is_above_character_limit(part, limit):
     return True if len(part) > limit else False
 
+
 print(is_email_valid("fakee&(((^mail@gmail.com"))
-print(is_email_valid("gakeadasfdsfdsafdsafdsasfdsdafadsfdsfdsafdsafdsafdsafdsafdsafdsaemail@gmail.com"))
+print(is_email_valid(
+    "gakeadasfdsfdsafdsafdsasfdsdafadsfdsfdsafdsafdsafdsafdsafdsafdsaemail@gmail.com"))
