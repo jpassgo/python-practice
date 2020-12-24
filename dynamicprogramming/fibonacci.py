fe17cfada0b1fd426748ee982f69b1a98e40b2ec
jpassgo <jeffpascoe9@gmail.com>
2020-12-24 12:58:47 -0600

    Now we solve fibonacci using memoization and dynamic programing.

--- dynamicprogramming/fibonacci.py
+++ dynamicprogramming/fibonacci.py
@@ -8,7 +8,26 @@ def fibonacci(n):
         result = 1
     else:
         result = fibonacci(n-1) + fibonacci(n-2)
-    return result
+    return result 
     
 
 print(fibonacci(10))
+
+def memoized_fibonacci(n, memo):
+    try:
+        if memo[n] != None:
+            return memo[n]
+    except:
+        pass
+    
+    if n == 1 or n == 2:
+        result = 1
+    else:
+        result = fibonacci(n-1) + fibonacci(n-2)
+    memo.insert(n, result)
+
+    return result
+
+memo = []
+print(memoized_fibonacci(10, memo))
+
