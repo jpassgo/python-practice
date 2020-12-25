3807e29a480c54fc8cccc129b170732338c9fe72
jpassgo <jeffpascoe9@gmail.com>
2020-12-25 13:38:58 -0600

    Bottom up dynamic programming.

--- dynamicprogramming/fibonacci.py
+++ dynamicprogramming/fibonacci.py
@@ -13,13 +13,15 @@ def fibonacci(n):
 
 print(fibonacci(10))
 
+
+# Recursion with Memoization
 def memoized_fibonacci(n, memo):
     try:
         if memo[n] != None:
             return memo[n]
     except:
         pass
-    
+
     if n == 1 or n == 2:
         result = 1
     else:
@@ -31,3 +33,17 @@ def memoized_fibonacci(n, memo):
 memo = []
 print(memoized_fibonacci(10, memo))
 
+# Bottom up approach
+def bottom_up_fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    bottom_up = [None for i in range(n+1)]
+    bottom_up[1] = 1
+    bottom_up[2] = 1
+
+    for i in range(3, n+1):
+        bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
+    
+    return bottom_up[n]
+
+print(bottom_up_fibonacci(10))
