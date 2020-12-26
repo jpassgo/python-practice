99744096749779e96150d986e75d4c9813fecfd6
jpassgo <jeffpascoe9@gmail.com>
2020-12-26 17:04:32 -0600

    Fixing formatting.

--- dynamicprogramming/fibonacci.py
+++ dynamicprogramming/fibonacci.py
@@ -1,15 +1,16 @@
 # Fibonacci Sequence:
 # 1, 1, 2, 3, 5, 8, 13, etc...
-# Starts with two 1's then sum the previous two numbers to calculate the next number in the sequence.
+# Starts with two 1's then sum the previous two numbers to calculate the
+# next number in the sequence.
 
 # Recursion
-def fibonacci(n): 
+def fibonacci(n):
     if n == 1 or n == 2:
         result = 1
     else:
-        result = fibonacci(n-1) + fibonacci(n-2)
-    return result 
-    
+        result = fibonacci(n - 1) + fibonacci(n - 2)
+    return result
+
 
 print(fibonacci(10))
 
@@ -17,33 +18,37 @@ print(fibonacci(10))
 # Recursion with Memoization
 def memoized_fibonacci(n, memo):
     try:
-        if memo[n] != None:
+        if memo[n] is not None:
             return memo[n]
-    except:
+    except BaseException:
         pass
 
     if n == 1 or n == 2:
         result = 1
     else:
-        result = fibonacci(n-1) + fibonacci(n-2)
+        result = fibonacci(n - 1) + fibonacci(n - 2)
     memo.insert(n, result)
 
     return result
+
 
 memo = []
 print(memoized_fibonacci(10, memo))
 
 # Bottom up approach
+
+
 def bottom_up_fibonacci(n):
     if n == 1 or n == 2:
         return 1
-    bottom_up = [None for i in range(n+1)]
+    bottom_up = [None for i in range(n + 1)]
     bottom_up[1] = 1
     bottom_up[2] = 1
 
-    for i in range(3, n+1):
-        bottom_up[i] = bottom_up[i-1] + bottom_up[i-2]
-    
+    for i in range(3, n + 1):
+        bottom_up[i] = bottom_up[i - 1] + bottom_up[i - 2]
+
     return bottom_up[n]
+
 
 print(bottom_up_fibonacci(10))
