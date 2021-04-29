a27da4e068038d01c451a32ecb9b0896ea8ba6e8
jpassgo <jeffpascoe9@gmail.com>
2021-04-28 21:37:19 -0500

    Updating merge sort implementation.

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,33 +1,63 @@
-def merge(array1, array2):
-    sorted_array = []
-    i, j = 0, 0
-    while i < len(array1) and j < len(array2):
-        if array1[i] < array2[j]:
-            sorted_array.append(array1[i])
-            i += 1
-        else:
-            sorted_array.append(array2[j])
-            j += 1
-    while i < len(array1):
-        sorted_array.append(array1[i])
+import numpy as np
+
+
+def merge_sort(arr, l, r):
+
+    if l < r:
+        mid = int((l + (r - 1)) / 2)
+        print(f"left: {l}, mid {mid}, right: {r}")
+
+        merge_sort(arr, l, mid)
+        merge_sort(arr, mid+1, r)
+        merge(arr, l, mid, r)
+
+
+
+def merge(arr, l, m, r):    
+    # print(f"merge = left: {l}, mid: {m}, right: {r}")
+
+    
+
+    left = []
+    i = l
+    while i < m:
+        left[i] = arr[l]
         i += 1
-    while j < len(array2):
-        sorted_array.append(array2[j])
+
+    right = []
+    i = m+1
+    while i < r:
+        right[i] = arr[i + 1]
+        i += 1
+
+    i = 0
+    j = 0
+    k = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1                  
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+        
+
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        arr[k] = right[j]
         j += 1
+        k += 1
 
-    return sorted_array
+        
 
-def merge_sort(array):
-    if len(array) < 2:
-        return array[:]
-    else:
-        middle = len(array)//2
-        l1 = merge_sort(array[:middle])
-        l2 = merge_sort(array[middle:])
-        return merge(l1, l2)
-
-
-l3 = [900, 89, 1, 323, 4, 5, 6, 8998]
-
-sortedList = merge_sort(l3)
-print(sortedList)
+    
+    
+arr = [900, 89, 1, 323, 4, 5, 6, 8998]
+print(arr)
+merge_sort(arr, 0, len(arr) - 1)
+print(arr)
