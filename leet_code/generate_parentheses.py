eb315a0f99410e2e77fcbf89ca02de8cd494b8e5
jpassgo <jeffpascoe9@gmail.com>
2021-10-11 19:30:37 -0500

    implementation of for a leetcode problem, I did not come up with. Copying it to understand it.

---
+++ leet_code/generate_parentheses.py
@@ -0,0 +1,30 @@
+
+def generate_parentheses():       
+    print(back_track())
+    return 1
+
+def back_track(well_form_parentheses = [], s = [], left = 0, right = 0, n: int = 3):
+    if len(s) == 2 * n:
+        well_form_parentheses.append("".join(s))
+        return
+    if left < n:
+        print(s)
+        print("\n")
+        s.append("(")
+        print(s)
+        print("\n")
+        back_track(well_form_parentheses, s, left+1, right, n)
+        s.pop()
+    if right < left:
+        print(s)
+        print("\n")
+        s.append(")")
+        print(s)
+        print("\n")
+        back_track(well_form_parentheses, s, left, right+1, n)
+        s.pop()
+    
+    return well_form_parentheses
+
+
+generate_parentheses()
