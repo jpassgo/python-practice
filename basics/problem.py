41cff836bf007f8b80c03d05a9683a193ac34f40
jpassgo <jeffpascoe9@gmail.com>
2021-10-13 20:57:52 -0500

    Solving a problem from CTCI.

---
+++ basics/problem.py
@@ -0,0 +1,22 @@
+
+def compress_string(val):
+    lc = 1
+    cc = ''
+
+    val_length = len(val)
+    for i in range(val_length):
+        if(i + 1 >= len(val) or (val[i] != val[i+1])):
+            cc += val[i] + str(lc)
+            lc = 1
+        else:
+            lc += 1  
+            
+
+    if(len(val) > len(cc)):
+        return cc
+    else:
+        return val
+
+
+
+print(compress_string('aaaabbbcccccdd'))
