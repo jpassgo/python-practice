d4760e7b71e7da173e71e7aa540715d747ea96de
jpassgo <jeffpascoe9@gmail.com>
2021-10-23 20:13:07 -0500

    Generating ListNode for testing the problem.

---
+++ leet_code/swap_list_nodes.py
@@ -0,0 +1,27 @@
+# 
+#  https://leetcode.com/problems/swap-nodes-in-pairs/
+# 
+#  24. Swap Nodes in Pairs
+# 
+#  Given a linked list, swap every two adjacent nodes and return its head. 
+#  You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+# 
+
+class ListNode:
+
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next        
+
+class Solution:
+
+    def swap_list_nodes(self, head: ListNode):
+        return
+        
+
+
+    
+
+nodes = [ListNode(i) for i in [1, 2, 3, 4]]
+
+print(print(nodes[0]))
