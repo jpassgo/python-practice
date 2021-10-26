e6ad2a4c07770ea1f2f52d17fcbfbb0c101c6c8a
jpassgo <jeffpascoe9@gmail.com>
2021-10-25 19:15:20 -0500

    Implementing swap list nodes.

--- leet_code/swap_list_nodes.py
+++ leet_code/swap_list_nodes.py
@@ -13,26 +13,55 @@ class ListNode:
         self.val = val
         self.next = next        
 
+
 class Solution:
 
     def swap_list_nodes(self, head: ListNode):
-        return
+        temp = head.next
+        swap(head, head.next)
+        return temp
+
+    def swap(self, current, next):
+        while current.next is not None:
+            temp = current
+            current = current.next
+            current.next = temp
+            swap(current.next, current.next.next)
+
+        return 
 
 
 
-
+# For testing the solution
 def populate_nodes(head_value = 1, size = 0):
     head = ListNode(head_value)
-    populate_nodes_recursive(head, head_value+1, size)
+    populate_nodes_recursively(head, head_value+1, size)
     return head
 
-def populate_nodes_recursive(previous, value, size):
-    previous.next = ListNode(value)
+def populate_nodes_recursively(previous, value, size):
+    current = ListNode(value)
+    previous.next = current
     if value >= size:
+        current.next = None
         return
     else:
-        populate_nodes_recursive(previous.next, value+1, size)
+        populate_nodes_recursively(previous.next, value+1, size)
 
 
 head = populate_nodes(size = 5)
 
+
+def recursively_print_nodes(node):
+    print(f'Node Value: {node.val}')
+    if node.next is None:
+        return 
+    else:
+        recursively_print_nodes(node.next)
+
+
+recursively_print_nodes(head)
+
+
+Solution().swap_list_nodes(head = head)
+
+recursively_print_nodes(head)
