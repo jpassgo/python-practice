cca88de62a597e6a27e085a26af5f01f4314b1fe
jpassgo <jeffpascoe9@gmail.com>
2021-10-30 14:42:41 -0500

    Solving the leet code problem.

--- leet_code/set_matrix_zeroes.py
+++ leet_code/set_matrix_zeroes.py
@@ -10,9 +10,46 @@
 from typing import List
 
 
+# 
+# [[1, 2, 3, 0],[1, 2, 1, 3],[1, 2, 3, 4],[1, 1, 2, 3, 4 ],[1, 2, 3, 4]]
+# 0, 3
+# 
+# 
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        
+        rows = []
+        columns = []
+        m, n = len(matrix), len(matrix[0])
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    rows.append(i)
+                    columns.append(j)
+
+        for i in range(m):  
+            for j in range(n):          
+                if i in rows:                
+                    matrix[i][j] = 0
+                if j in columns:
+                    matrix[i][j] = 0            
+                
+
+def print_values(matrix):
+    m, n = len(matrix), len(matrix[0])
+    for i in range(m):        
+        for j in range(n):
+            print(matrix[i][j], end='')
+        print('')
+                
+
+solution = Solution()
+matrix = [[1, 2, 3, 0],[1, 2, 1, 3],[1, 2, 3, 4],[1, 1, 2, 3, 4 ],[1, 2, 3, 4]]
+
+print_values(matrix)
+
+solution.setZeroes(matrix)
+
+print('********************')
+
+print_values(matrix)
