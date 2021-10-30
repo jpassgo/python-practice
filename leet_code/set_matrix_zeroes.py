e89d02fd778a27b0a5e86290b004d78ac0477850
jpassgo <jeffpascoe9@gmail.com>
2021-10-29 21:52:37 -0500

    Adding template for next problem to solve.

---
+++ leet_code/set_matrix_zeroes.py
@@ -0,0 +1,18 @@
+# 
+# 73. Set Matrix Zeroes
+# 
+# Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix.
+# You must do it in place.
+# 
+# 
+# https://leetcode.com/problems/set-matrix-zeroes/
+# 
+from typing import List
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
