87729e53a14e196de3ac20177d9c1649c555dc6c
jpassgo <jeffpascoe9@gmail.com>
2021-11-14 11:21:43 -0600

    Another leetcode problem.

--- leet_code/sort_by_frequency.py
+++ leet_code/sort_by_frequency.py
@@ -8,8 +8,46 @@
 # https://leetcode.com/problems/sort-characters-by-frequency/
 # 
 
+# input = acbaabd
+
+# frequency dict
+{'a': 3, 'c': 1, 'b': 2, 'd': 1}
+
+# permutation dict
+{3: ['a'], 2: ['b'], 1: ['c', 'd']}
+
 class Solution:
     def frequencySort(self, s: str) -> str:
-        frequency_map = {}
+        frequency_dict = {}
 
-        for
+        for c in s:
+            if c in frequency_dict.keys():
+                frequency_dict[c] = frequency_dict[c]+1                
+            else:
+                frequency_dict[c] = 1
+
+        permutation_dict = {}
+        for k, v in frequency_dict.items():
+            if v in permutation_dict.keys():
+                permutation_dict[v].append(k)             
+            else:
+                permutation_dict[v] = [k]
+    
+        permutation_dict = sorted(permutation_dict.items(), reverse=True)
+        
+        sorted_string = ''
+        for k in permutation_dict:
+            for v in k[1]:
+                sorted_string += v*k[0]
+
+        return sorted_string
+
+        
+
+
+
+solution = Solution()
+
+print(solution.frequencySort('acbaabd'))
+print(solution.frequencySort('Acbaabd'))
+        
