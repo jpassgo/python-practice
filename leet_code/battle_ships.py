15446492102984e192317d00a252d81042d5e4a0
jpassgo <jeffpascoe9@gmail.com>
2021-11-15 21:04:37 -0600

    Adding template

---
+++ leet_code/battle_ships.py
@@ -0,0 +1,31 @@
+# 
+# 419. Battleships in a Board
+# 
+# Given an m x n matrix board where each cell is a battleship 'X' or empty '.', return the number of the battleships on board. 
+# Battleships can only be placed horizontally or vertically on board. 
+# In other words, they can only be made of the shape 1 x k (1 row, k columns) or k x 1 (k rows, 1 column), where k can be of any size. 
+# At least one horizontal or vertical cell separates between two battleships (i.e., there are no adjacent battleships).
+# 
+# https://leetcode.com/problems/battleships-in-a-board/
+# 
+
+from typing import List
+
+class Solution:
+    def countBattleships(self, board: List[List[str]]) -> int:
+        battle_ship_tracker = {}
+
+        for item in board:
+            
+
+
+
+
+
+
+
+board = [["X",".",".","X"],[".",".",".","X"],[".",".",".","X"]]
+
+solution = Solution()
+
+assert solution.countBattleships(board)
