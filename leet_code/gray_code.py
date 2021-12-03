11050e89543982d5b4a9dfaf305b1439788b53ca
jpassgo <jeffpascoe9@gmail.com>
2021-12-02 21:11:25 -0600

    Leetcode problem

---
+++ leet_code/gray_code.py
@@ -0,0 +1,26 @@
+# 
+# 89. Gray Code
+# 
+# An n-bit gray code sequence is a sequence of 2n integers where:
+# Every integer is in the inclusive range [0, 2n - 1],
+# The first integer is 0,
+# An integer appears no more than once in the sequence,
+# The binary representation of every pair of adjacent integers differs by exactly one bit, and
+# The binary representation of the first and last integers differs by exactly one bit.
+# Given an integer n, return any valid n-bit gray code sequence.
+# 
+# https://leetcode.com/problems/gray-code/
+# 
+
+from typing import List
+
+class Solution:
+    def gray_code(self, n: int) -> List[int]:
+        
+        
+
+    def generate_sequence(sequence: List[int])
+
+
+solution = Solution()
+solution.grayCode()
