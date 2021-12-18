f31db40030148d8b1b34495b14ed0cd86fa80d53
jpassgo <jeffpascoe9@gmail.com>
2021-12-17 19:18:52 -0600

    Checking if a sub tree is balanced

--- searching/bst_balance.py
+++ searching/bst_balance.py
@@ -14,6 +14,18 @@ def height(node):
 
     return 1 + max(left_height, right_height)
 
+
+def is_balanced(node):
+    if node is None:
+        return -1
+    else:
+        left_height = is_balanced(node.left)
+        right_height = is_balanced(node.right)
+        if abs(left_height - right_height) > 1:
+            return False
+
+    return 1 + max(left_height, right_height)
+
 root = TreeNode(10, TreeNode(7, TreeNode(6), TreeNode(8)), TreeNode(13, TreeNode(11), TreeNode(15)))
 
 print(height(root))
