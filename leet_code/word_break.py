746543b8f3fa28024dd5e71861fd1a24f81b2696
jpassgo <jeffpascoe9@gmail.com>
2021-12-27 18:36:27 -0600

    Setting up problem template

---
+++ leet_code/word_break.py
@@ -0,0 +1,13 @@
+# 
+# 139. Word Break
+# 
+# Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a 
+# space-separated sequence of one or more dictionary words.
+# Note that the same word in the dictionary may be reused multiple times in the segmentation.
+# 
+# https://leetcode.com/problems/word-break/
+# 
+
+
+def wordBreak(s: str, wordDict: List[str]) -> bool:
+    return False
