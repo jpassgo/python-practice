c439ce46844ce52c01c0fa378a2660ac8fa35470
jpassgo <jeffpascoe9@gmail.com>
2021-12-27 20:49:57 -0600

    Finishing implementation. Using trie to solve the problem.

--- leet_code/word_break.py
+++ leet_code/word_break.py
@@ -7,7 +7,37 @@
 # 
 # https://leetcode.com/problems/word-break/
 # 
+from typing import List
 
-
+# The solution for this should be to use a trie
 def wordBreak(s: str, wordDict: List[str]) -> bool:
-    return False
+    trie = {'root': {}}
+    for word in wordDict:
+        node = trie['root']
+        for character in word:
+            if character in node.keys():
+                break                                
+            else:
+                node[character] = {}                
+            node = node[character]
+    
+    node = trie['root']
+    i = 0
+    s_len = len(s)
+    while i < s_len:        
+        if s[i] in node.keys():                     
+            node = node[s[i]]
+            s = s.replace(s[i], '', 1)
+            s_len = len(s)                       
+        elif len(node.keys()) == 0:
+            node = trie['root']                    
+        else:
+            return False
+        
+    return True
+
+print(wordBreak('leetcode', ['leet', 'code']))
+
+print(wordBreak('applepenapple', ['apple', 'pen']))
+
+print(wordBreak('catsandog', ['cats', 'dog', 'sand', 'and', 'cat']))
