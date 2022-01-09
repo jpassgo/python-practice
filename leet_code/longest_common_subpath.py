8bb83503d92953a34e0a7b09444de38c63ae2ec3
jpassgo <jeffpascoe9@gmail.com>
2022-01-08 23:52:26 -0600

    Starting implementation. Need a way to  determing all of the common paths. BFS?

--- leet_code/longest_common_subpath.py
+++ leet_code/longest_common_subpath.py
@@ -13,3 +13,54 @@
 # 
 # https://leetcode.com/problems/longest-common-subpath/
 # 
+from typing import List
+
+class Node:
+    
+    def __init__(self, value=None, children=[], count=0):
+        self.value = value
+        self.children = children
+        self.count = count
+
+    def get(self, value):
+        for child in self.children:
+            if child.value == value:
+                return child
+
+    def __repr__(self):
+        return f'value={value}, count={count}, children={children}'
+
+def longestCommonSubpath(n: int, paths: List[List[int]]) -> int:
+    longest_path = 0
+    root = Node()
+    for path in paths:
+        i = 0
+        path_len = len(path)
+        while i < path_len:
+            longest_path = recursively_add_values(root, path, i)
+            i += 1
+    
+    return longest_path
+
+def recursively_add_values(node, paths: List[List[int]], i):
+    if i < len(paths)-1:
+        if paths[i] in node.children:
+            return recursively_add_values(node.get(paths[i]), paths, i+1)
+        else:
+            node.children.append(Node(paths[i]))
+            return recursively_add_values(node.get(paths[i]), paths, i+1)
+    elif paths[i] in node.children:
+        for current_node in node.children:
+            if current_node.value == paths[i]:
+                current_node.count += 1
+                return current_node.count
+    else:
+        return 0
+
+
+n = 5
+paths = [[0,1,2,3,4],
+                       [2,3,4],
+                       [4,0,1,2,3]]
+
+print(longestCommonSubpath(n, paths))
