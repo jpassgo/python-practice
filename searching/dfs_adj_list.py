a48a01d8436038944eb7ba0ea67d81a06da69733
jpassgo <jeffpascoe9@gmail.com>
2022-01-15 15:42:56 -0600

    depth first search using recursion and adj list.

---
+++ searching/dfs_adj_list.py
@@ -0,0 +1,20 @@
+
+# using an adjacency list to represent the graph
+
+def dfs(graph, node=0):
+    visited = set()
+
+    recursive_bfs(graph, visited, node)
+
+def recursive_dfs(graph, visited, node):
+    print(node)
+
+    visited.add(node)
+
+    for v in graph[node]:
+        if v not in visited:
+            recursive_dfs(graph, visited, v)
+    
+
+
+dfs([[1,2], [0,2,4], [0,1,3], [2], [1]])
