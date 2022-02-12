c2b5ce50dd20cbafc30088cac1ee6621a9bcb092
jpassgo <jeffpascoe9@gmail.com>
2022-02-12 09:03:47 -0600

    Find the Kth largest value in BST

---
+++ leet_code/kth_largest_bst_value.py
@@ -0,0 +1,29 @@
+
+# 
+# Find the Kth largest value in BST
+# 
+# 
+
+
+# This is an input class. Do not edit.
+class BST:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def findKthLargestValueInBst(tree, k):
+    sorted_list = depth_first_sorting(tree, k)	
+    return sorted_list[len(sorted_list) - k].value
+
+
+def depth_first_sorting(node, k, list=[]):
+	if node is None:
+		return
+	else:				
+		depth_first_sorting(node.left, list)
+		list.append(node)
+		depth_first_sorting(node.right, list)
+	
+	return list
+		
