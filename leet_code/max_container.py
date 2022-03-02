099a8ec538a2f968d3af868d54798d205c7d53fb
jpassgo <jeffpascoe9@gmail.com>
2022-03-01 19:18:42 -0600

    Max container problem

---
+++ leet_code/max_container.py
@@ -0,0 +1,16 @@
+
+def maxArea(height: List[int]) -> int:        
+    i, j = 0, len(height) - 1
+    max_area = float('-inf')
+                                    
+    while i < j:                     
+                    
+        max_area = max(max_area, min(height[i], height[j]) * (j - i))         
+        if (height[i] < height[j]):
+            i += 1
+        else:
+            j -= 1
+        
+    return max_area
+        
+            
