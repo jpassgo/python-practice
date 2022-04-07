27f13f81ecb4c06b5bf1a3750b9404ab4076a63b
jpassgo <jeffpascoe9@gmail.com>
2022-04-06 21:10:40 -0500

    Fibonacci using memoization

---
+++ leet_code/fib.py
@@ -0,0 +1,26 @@
+# f0 = 0
+# f1 = 1
+# f2 = 1
+# f3 = 2
+# f4 = 3
+# f5 = 5
+# f6 = 8
+# f7 = 13
+
+def getNthFib(n):
+	return fib(n)
+	
+
+def fib(n, memo={}):
+	if n == 1:
+		return 0
+	elif n == 2:
+		return 1
+	
+	memo[n] = fib(n-2) + fib(n-1)
+	
+	return memo[n]
+
+
+
+print(getNthFib(6))
